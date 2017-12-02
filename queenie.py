209c8f57c53adf70f9fa2fa4ba67afcdb8fefcb1
queeniexiang <wxiang1@stuy.edu>
2017-12-02 13:57:30 -0500

    Python and flask stuff added

---
+++ queenie.py
@@ -0,0 +1,29 @@
+from flask import Flask, session, url_for, redirect, render_template, request
+import urllib2
+import requests
+import json
+
+
+#App instantiation
+app = Flask(__name__)
+#app.secret_key = os.urandom(32)
+
+
+
+@app.route("/")
+def homepage():
+    return redirect("/questions") 
+
+@app.route("/questions")
+def questions():
+    return render_template("questions.html")
+
+@app.route("/results")
+def results():
+    sentence = request.args['sentence'] 
+    return render_template("results.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
